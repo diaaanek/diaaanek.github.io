c5491fbdcc90584d718d8a9b82f0e4c2164fd426
diaaanek <dkorongy@gmail.com>
2019-03-28 13:56:47 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -1,145 +1,123 @@
-
-
-// The typewriter element
-var typeWriterElement = document.getElementById('typewriter');
-
-// The TextArray:
-var textArray = ["building beautiful interfaces","inspiring women in tech. ", "feeding my artistic spirit."];
-
-// function to generate the backspace effect
-function delWriter(text, i, cb) {
-	if (i >= 0 ) {
-		typeWriterElement.innerHTML = text.substring(0, i--);
-		// generate a random Number to emulate backspace hitting.
- 		var rndBack = 10 + Math.random() * 100;
-		setTimeout(function() {
-			delWriter(text, i, cb);
-		},rndBack);
-	} else if (typeof cb == 'function') {
-		setTimeout(cb,1000);
-	}
-};
-
-// function to generate the keyhitting effect
-function typeWriter(text, i, cb) {
-	if ( i < text.length ) {
-		typeWriterElement.innerHTML = text.substring(0, i++);
-		// generate a random Number to emulate Typing on the Keyboard.
-		var rndTyping = 250 - Math.random() * 100;
-		setTimeout( function () {
-			typeWriter(text, i++, cb)
-		},rndTyping);
-	} else if (i === text.length) {
-		setTimeout( function () {
-			delWriter(text, i, cb)
-		},1000);
-	}
-};
-
-// the main writer function
-function StartWriter(i) {
-	if (typeof textArray[i] == "undefined") {
-		setTimeout( function () {
-			StartWriter(0)
-		},1000);
-	} else if(i < textArray[i].length) {
-		typeWriter(textArray[i], 0, function () {
-			StartWriter(i+1);
-		});
-	}
-};
-// wait one second then start the typewriter
-setTimeout( function () {
-	StartWriter(0);
-},1000);
-
-
-function toggleNightMode() {
-  var body = document.getElementById("body");
-  var currentClass = body.className;
-  body.className = currentClass == "night-mode" ? "light-mode" : "night-mode";
-}
-
-const toggle = document.querySelector('.toggle');
-
-toggle.addEventListener('click', function(){
-  document.body.classList.toggle('on');
-})
-
-$(function() {
-  const d = new Date();
-  const hours = d.getHours();
-  const night = hours >= 19 || hours <= 7; // between 7pm and 7am
-  const body = document.querySelector('body');
-  const toggle = document.getElementById('toggle');
-  const input = document.getElementById('switch');
-
-  if (night) {
-    input.checked = true;
-    body.classList.add('night');
-  }
-
-  toggle.addEventListener('click', function() {
-    const isChecked = input.checked;
-    if (isChecked) {
-      body.classList.remove('night');
-    } else {
-      body.classList.add('night');
-    }
-  });
-
-  const introHeight = document.querySelector('.intro').offsetHeight;
-  const topButton = document.getElementById('top-button');
-  const $topButton = $('#top-button');
-
-  window.addEventListener(
-    'scroll',
-    function() {
-      if (window.scrollY > introHeight) {
-        $topButton.fadeIn();
-      } else {
-        $topButton.fadeOut();
+"use strict";
+var isFirefox = "undefined" != typeof InstallTrigger,
+  mobileVar = isMobile.any;
+$(".bg-cover").imagesLoaded(
+  {
+    background: !0
+  },
+  function(e) {
+    TweenMax.fromTo(
+      $(".bg-cover"),
+      0.25,
+      {
+        autoAlpha: 0
+      },
+      {
+        autoAlpha: 1
       }
-    },
-    false
-  );
-
-  topButton.addEventListener('click', function() {
-    $('html, body').animate({ scrollTop: 0 }, 500);
-  });
-
-  const hand = document.querySelector('.emoji.wave-hand');
-
-  function waveOnLoad() {
-    hand.classList.add('wave');
-    setTimeout(function() {
-      hand.classList.remove('wave');
-    }, 2000);
+    );
   }
-
-  setTimeout(function() {
-    waveOnLoad();
-  }, 1000);
-
-  hand.addEventListener('mouseover', function() {
-    hand.classList.add('wave');
+),
+  $(window).load(function() {
+    function e() {
+      o.countdown("resume");
+    }
+    var n = "#EC008C";
+    $("#minimos-4").length && (n = "#F8EB31"),
+      $("#drifter").length
+        ? initDrifter("drifter", n)
+        : $("#particles-js").length && initStompParticles();
+    var o = $(".st-countdown");
+    o.countdown("2017/09/01", function(e) {
+      $(this).html(e.strftime("%D Days %H:%M:%S"));
+    }),
+      $(".btn-count").hover(
+        function() {
+          o.countdown("pause"), $(".st-countdown").html("Bid on Flippa");
+        },
+        function() {
+          o.countdown("resume");
+        }
+      );
+    var a,
+      t,
+      s = $(".main-content p"),
+      r = $(".main-content h3"),
+      i = $(".main-content ul"),
+      c = "20px",
+      l = new TimelineMax({
+        paused: !0,
+        delay: 0.05,
+        onComplete: function() {
+          i.length &&
+            TweenMax.to(i, 0.75, {
+              autoAlpha: 1,
+              ease: Linear.easeNone
+            });
+        },
+        onCompleteParams: []
+      });
+    isFirefox ? (c = "0px") : mobileVar && (c = "2px"),
+      o.countdown("pause"),
+      r.length &&
+        ((t = new SplitText(r, {
+          type: "words,chars",
+          wordsClass: "gpu-hack sven-word-++",
+          charsClass: "sven-char-++"
+        })),
+        l.staggerFromTo(
+          t.chars,
+          0.4,
+          {
+            y: "-50%",
+            opacity: 0
+          },
+          {
+            y: "0%",
+            opacity: 1,
+            ease: Bounce.easeOut
+          },
+          0.08
+        ),
+        l.addLabel("end"),
+        l.call(e, [])),
+      s.length &&
+        ((a = new SplitText(s, {
+          type: "words,chars",
+          wordsClass: "gpu-hack s-word sven-word-++",
+          charsClass: "s-char sven-char-++"
+        })),
+        TweenMax.set(a.chars, {
+          force3D: !0,
+          rotationZ: "0.01deg"
+        }),
+        $.each(a.chars, function(e, n) {
+          var o = $(n).css("color"),
+            a = "end+=" + 0.04 * e;
+          l.fromTo(
+            $(n),
+            0.8,
+            {
+              opacity: 0,
+              textShadow: "0 0 " + c + " " + o,
+              scale: 1.2,
+              color: "transparent"
+            },
+            {
+              opacity: 1,
+              scale: 1,
+              color: o,
+              textShadow: "none",
+              ease: Linear.easeNone
+            },
+            a
+          );
+        })),
+      TweenMax.set($(".main-content"), {
+        visibility: "visible"
+      }),
+      TweenMax.set($(".preloader"), {
+        display: "none"
+      }),
+      l.play();
   });
-
-  hand.addEventListener('mouseout', function() {
-    hand.classList.remove('wave');
-  });
-
-  window.sr = ScrollReveal({
-    reset: false,
-    duration: 600,
-    easing: 'cubic-bezier(.694,0,.335,1)',
-    scale: 1,
-    viewFactor: 0.3,
-  });
-
-  sr.reveal('.background');
-  sr.reveal('.skills');
-  sr.reveal('.experience', { viewFactor: 0.2 });
-  sr.reveal('.featured-projects', { viewFactor: 0.1 });
-  sr.reveal('.other-projects', { viewFactor: 0.05 });
- });
