b5bdea759cd3843ab0bea3b31dd3fdf7ee7e939f
diaaanek <dkorongy@gmail.com>
2019-06-12 21:35:07 -0400

    scroll to top

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,7 +10,7 @@ let closeIcon = document.querySelector('.mobile-close-icon');
 let mobileNav = document.getElementById('mobileNav');
 
 ///////////////////////////////////////
-// STICKY NAV BAR 
+// STICKY NAV BAR
 ///////////////////////////////////////
 
 window.onscroll = function() {
@@ -41,4 +41,22 @@ function openNav() {
 
 function closeNav() {
   mobileNav.style.height = "0%";
+}
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
