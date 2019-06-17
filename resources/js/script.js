ef7d6a91e003f33025c975a328604c00b5aa9b6b
diaaanek <dkorongy@gmail.com>
2019-06-17 10:44:50 -0400

    Fixed projects

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,18 +4,18 @@
 // GETTING ELEMENTS FROM DOM
 ///////////////////////////////////////
 let nav = document.getElementById("main-nav");
-let header = document.querySelector('#header');
-let navIcon = document.querySelector('.mobile-nav-icon');
-let closeIcon = document.querySelector('.mobile-close-icon');
-let mobileNav = document.getElementById('mobileNav');
+let header = document.querySelector("#header");
+let navIcon = document.querySelector(".mobile-nav-icon");
+let closeIcon = document.querySelector(".mobile-close-icon");
+let mobileNav = document.getElementById("mobileNav");
 
 ///////////////////////////////////////
 // STICKY NAV BAR
 ///////////////////////////////////////
 
 window.onscroll = function() {
-    makeSticky();
-}
+  makeSticky();
+};
 
 function makeSticky() {
   if (window.pageYOffset >= header.offsetHeight) {
@@ -29,11 +29,10 @@ function makeSticky() {
 // MOBILE NAV
 ///////////////////////////////////////
 
-
 /* Open when someone clicks on the span element */
 
-navIcon.addEventListener('click', openNav);
-closeIcon.addEventListener('click', closeNav);
+navIcon.addEventListener("click", openNav);
+closeIcon.addEventListener("click", closeNav);
 
 function openNav() {
   mobileNav.style.height = "100%";
@@ -43,9 +42,10 @@ function closeNav() {
   mobileNav.style.height = "0%";
 }
 
-
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -61,19 +61,39 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-
 // IMAGE CAROUSEL
 
-var imgList = document.getElementById('imgList');
-var scrollRight = document.getElementById('scroll-right');
-var scrollLeft = document.getElementById('scroll-left');
+var imgList = document.getElementById("imgList");
+var scrollRight = document.getElementById("scroll-right");
+var scrollLeft = document.getElementById("scroll-left");
 
 // When a user clicks on the right arrow, the ul will scroll 750px to the right
-scrollRight.addEventListener('click', (event) => {
+scrollRight.addEventListener("click", event => {
   imgList.scrollBy(750, 0);
 });
 
 // When a user clicks on the left arrow, the ul will scroll 750px to the left
-scrollLeft.addEventListener('click', (event) => {
+scrollLeft.addEventListener("click", event => {
   imgList.scrollBy(-750, 0);
 });
+
+// TOGGLE SWITCH
+
+var checkbox = document.querySelector("input[name=theme]");
+
+checkbox.addEventListener("change", function() {
+  if (this.checked) {
+    trans();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    trans();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+let trans = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
